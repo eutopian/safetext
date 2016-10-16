6993f34f7515ce27eb67a9907e76c528eaa49925
Derick Quartey <niikwartey1@gmail.com>
2016-10-15 20:08:35 -0400

    colored marker

--- app/models/civilian.rb
+++ app/models/civilian.rb
@@ -2,4 +2,8 @@ class Civilian < ActiveRecord::Base
   has_many :civilian_questions
   has_many :questions, through: :civilian_questions
   has_many :answers
+
+  def self.addresses
+    self.all.map { |civilian|  civilian.address }
+  end 
 end
